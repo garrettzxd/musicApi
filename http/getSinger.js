1a711db89e87df42a6055f424188ff718236dbf6
garrett <943253628@qq.com>
2018-12-18 10:56:33 +0800

    我还是改成直接从qq拿数据吧😂

---
+++ http/getSinger.js
@@ -0,0 +1,65 @@
+let http = require('../common/http');
+let {dataProcessing} = require('../common/currency');
+const SINGLE_PAGE_NUM = 80;
+const BASE_URL = 'https://u.y.qq.com/cgi-bin/musicu.fcg?';
+const BASE_DATA = {
+    callback: '',
+    g_tk: 5381,
+    jsonpCallback: '',
+    loginUin: 0,
+    hostUin: 0,
+    format: 'jsonp',
+    inCharset: 'utf8',
+    outCharset: 'utf-8',
+    notice: 0,
+    platform: 'yqq',
+    needNewCode: 0,
+    data: ''
+};
+
+async function singeRequest(params) {
+    let url = _setUrl();
+    let res = await http(url);
+    res = dataProcessing(res);
+    return res;
+}
+
+/**
+ * [_setUrl 动态生产请求URL]
+ * @param  {[Number]} pageNumber [页码]
+ * @param  {number} sex [性别：-100(all)、0(male)、1(female)、2(band)]
+ * @param  {number} genre [音乐类型：-100(默认)、1(流行)、2(摇滚)、3(民谣)、4(电子)、5(爵士)、6(嘻哈)、8(R&G)、9(轻音乐)、10(民歌)、20(蓝调)、25(乡村)]
+ * @return {string}            [生成的URL]
+ */
+function _setUrl({page = 1, area = -100, sex = -100, genre = -100, index = -100} = {}) {
+    let result_url = BASE_URL;
+    let son_data = {
+        "comm":{
+            "ct":24,
+            "cv":10000
+        },
+        "singerList":{
+            "module":"Music.SingerListServer",
+            "method":"get_singer_list",
+            "param":{
+                "area": area,
+                "sex": sex,
+                "genre": genre,
+                "index": index,
+                "sin": (page - 1)*SINGLE_PAGE_NUM,
+                "cur_page": page
+            }
+        }
+    };
+    let name = "getUCGI" + (Math.random() + "").replace("0.", "");
+    BASE_DATA.callback = name;
+    BASE_DATA.jsonpCallback = name;
+    BASE_DATA.data = encodeURIComponent(JSON.stringify(son_data));
+
+    for(let key in BASE_DATA) {
+        result_url = result_url + `${key}=${BASE_DATA[key]}&`;
+    }
+    return result_url;
+}
+
+module.exports = singeRequest;
--- routes/singer.js
+++ routes/singer.js
@@ -1,54 +1,18 @@
 let express = require('express');
 let router = express.Router();
-let paradigm = require('../common/Paradigm');
-let {SUCCESS, MISS_PARAMETER, UN_KNOW_ERROR} = require('../common/ErrorContans');
-let {
-    getSingerList,
-    getSingerSongList
-} = require('../model/singer-model');
-
+let getSinger = require('../http/getSinger');
 //获取歌手列表
 router.get('/singerList', (req, res, next) => {
-    //do something
-    getSingerList(req.query).then((data) => {
-        console.log('data is:',data);
-        let result = paradigm({
-            message: SUCCESS,
-            data: Object.values(data)
-        });
-        res.json(result);
+    getSinger(req.query).then((data) => {
+        res.json(data);
     }).catch((err) => {
-        res.json(err);
-    })
+        res.json({err: 'occur an error!'});
+    });
 });
 
 //根据歌手mid获取歌手歌曲列表
 router.get('/singerSongList', (req, res, next) => {
-    let {singer_mid} = req.query;
-    if (!singer_mid) {
-        let result = paradigm({
-            message: MISS_PARAMETER,
-            data: {}
-        });
-        res.json(result);
-    }else {
-        next()
-    }
-}, (req, res, next) => {
-    getSingerSongList(req.query).then((data) => {
-        let result = paradigm({
-            message: SUCCESS,
-            data: Object.values(data)
-        });
-        res.json(result);
-    }).catch((err) => {
-        console.log(err);
-        let result = paradigm({
-            message: UN_KNOW_ERROR,
-            data: err
-        });
-        res.json(result);
-    })
+
 });
 
 //获取歌手描述
