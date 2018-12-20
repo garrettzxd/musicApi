a5bfde55ce27081442d0e015f52e6968fae0864c
garrett <943253628@qq.com>
2018-12-20 23:14:12 +0800

    del useless

--- common/errorContans.js
+++
@@ -1,9 +0,0 @@
-const SUCCESS = {code: 0, error: 'success'};
-const MISS_PARAMETER = {code: 1, error: '缺少必传参数'};
-const UN_KNOW_ERROR = {code: 2, error: '未知错误'};
-
-module.exports = {
-    SUCCESS,
-    MISS_PARAMETER,
-    UN_KNOW_ERROR
-};
